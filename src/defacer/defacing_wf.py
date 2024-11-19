b5b82b7c31a5057844a3526b873956bddf673774
Victor Nozais <vnozais@fealinx.com>
2024-11-19 09:35:58 +0100

    Finishing the wf with the sink, adding click for cli support, and thread limit

--- src/defacer/defacing_wf.py
+++ src/defacer/defacing_wf.py
@@ -1,24 +1,36 @@
 from pathlib import Path
 from nipype.pipeline.engine import Workflow, Node, JoinNode
 from nipype.interfaces.utility import IdentityInterface, Function
-from nipype.interfaces.io import DataGrabber
+from nipype.interfaces.io import DataGrabber, DataSink
 from nipype.interfaces.dcm2nii import Dcm2niix
 from nipype.interfaces.quickshear import Quickshear
 from defacer.mask_creation import gen_mask_wf
+import click
 
 
-def main(inDir: Path, outDir: Path, threads4mask: int):
+@click.command()
+@click.option('--indir', type=click.Path(exists=True), help='Folder (directory) containing all the DICOM series (in sub-folders).')
+@click.option('--outdir', type=click.Path(exists=False), help='Folder (directory) where the results will be stored.')
+@click.option('--threads4mask', default=4, help='Number of CPUs to use for the segmentation of the brain (needed for the defacing).')
+@click.option('--modeldir', type=click.Path(exists=True), default='/data/model', help='Folder (directory) where the IA model for the brain masking is stored.')
+@click.option('--descriptor', type=click.Path(exists=True), default='/data/model/descriptor.json', help='File (.json) describing the info about the AI model.')
+def main(indir: Path, outdir: Path, threads4mask: int, modeldir: Path, descriptor: Path):
 
-    if isinstance(inDir, str):
-        inDir = Path(inDir)
-    if isinstance(outDir, str):
-        outDir = Path(outDir)
+    if isinstance(indir, str):
+        indir = Path(indir).absolute()
+    if isinstance(outdir, str):
+        outdir = Path(outdir).absolute()
 
-    dcm_folder_list = [dcmdir.stem for dcmdir in inDir.iterdir() if dcmdir.is_dir()]
+    print("indir : " + str(indir))
+
+    if not outdir.exists():
+        Path.mkdir(outdir)
+
+    dcm_folder_list = [dcmdir.stem for dcmdir in indir.iterdir() if dcmdir.is_dir()]
 
     wf_name = 'defacer_wf'
     workflow = Workflow(wf_name)
-    workflow.base_dir = outDir
+    workflow.base_dir = outdir
 
     folder_iterator = Node(
         IdentityInterface(
@@ -31,7 +43,7 @@ def main(inDir: Path, outDir: Path, threads4mask: int):
         infields=['aquisition_id'],
         outfields=['aquisition_folder']),
         name='datagrabber')
-    datagrabber.inputs.base_directory = inDir
+    datagrabber.inputs.base_directory = indir
     datagrabber.inputs.raise_on_empty = True
     datagrabber.inputs.sort_filelist = True
     datagrabber.inputs.template = '%s'
@@ -46,11 +58,27 @@ def main(inDir: Path, outDir: Path, threads4mask: int):
 
     workflow.connect(datagrabber, 'aquisition_folder', dcm2nii, 'source_dir')
 
-    mask_wf = gen_mask_wf(threads4mask)
+    mask_wf = gen_mask_wf(threads4mask, model=modeldir, descriptior=descriptor)
     workflow.add_nodes([mask_wf])
 
     defacing = Node(Quickshear(), name='defacing')
 
-    workflow.connect(dcm2nii, 'converted_files', mask_wf, 'preconform.img')
+    workflow.connect(dcm2nii, 'converted_files', mask_wf, 'crunch.img')
     workflow.connect(dcm2nii, 'converted_files', defacing, 'in_file')  # For now, expect 1 file only from dcm2nii ouput, also, the dcm2nii.bids (json) sidecar
     workflow.connect(mask_wf, 'binarize_brain_mask.thresholded', defacing, 'mask_file')
+
+    sink_node = Node(DataSink(),
+                     name='sink_node')
+    sink_node.inputs.base_directory = str(outdir / 'results')
+    sink_node.inputs.substitutions = [
+        ('_aquisition_id_', '')
+    ]
+    workflow.connect(defacing, 'out_file', sink_node, 'defaced_images')
+    workflow.connect(dcm2nii, 'bids', sink_node, 'defaced_images.@json')
+
+    workflow.config['execution']['stop_on_first_crash'] = 'True'  # For debug
+    workflow.run()
+
+
+if __name__ == '__main__':
+    main()
